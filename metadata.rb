a7d621c1bf31bc981a2e35e4e7b93647adf5fe66
Matt Thompson <mattt@defunct.ca>
2013-02-20 12:37:42 +0000

    Cleanup of documentation and metadata files

--- metadata.rb
+++ metadata.rb
@@ -3,26 +3,30 @@ license           "Apache 2.0"
 description       "Installs and configures Openstack"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.19"
-recipe		  "api-ec2", ""
-recipe		  "api-metadata", ""
-recipe		  "api-os-compute", ""
-recipe		  "api-os-volume", ""
-recipe		  "compute", ""
-recipe		  "default", ""
-recipe		  "libvirt", ""
-recipe		  "network", ""
-recipe		  "nova-common", ""
-recipe		  "nova-rsyslog", ""
-recipe		  "nova-scheduler-patch", ""
-recipe		  "nova-setup", ""
-recipe		  "scheduler", ""
-recipe		  "vncproxy", ""
-recipe		  "volume", ""
+recipe		        "api-ec2", ""
+recipe		        "api-metadata", ""
+recipe		        "api-os-compute", ""
+recipe		        "api-os-volume", ""
+recipe		        "compute", ""
+recipe		        "default", ""
+recipe            "essex-final-volume", ""
+recipe            "folsom-volume", ""
+recipe		        "libvirt", ""
+recipe            "nova-cert", ""
+recipe		        "nova-common", ""
+recipe		        "nova-rsyslog", ""
+recipe		        "nova-scheduler-patch", ""
+recipe		        "nova-setup", ""
+recipe		        "scheduler", ""
+recipe		        "vncproxy", ""
+recipe		        "volume", ""
 
-%w{ ubuntu fedora redhat centos }.each do |os|
+%w{ centos ubuntu }.each do |os|
   supports os
 end
 
-%w{ apt cinder database dsh glance keystone monitoring mysql openssh rabbitmq selinux nova-network osops-utils sysctl yum }.each do |dep|
+%w{ cinder database dsh monitoring mysql nova-network openssl osops-utils sysctl }.each do |dep|
   depends dep
 end
+
+depends "keystone", ">= 1.0.17"
