2e6864c1b6eed151664e28bceb4e514d43c3df64
cloudnull <kevin.carter@rackspace.com>
2013-07-05 15:32:58 -0500

    updated index creation to be run as a library method.

--- recipes/nova-setup.rb
+++ recipes/nova-setup.rb
@@ -57,27 +57,24 @@ execute "nova-manage db sync" do
   #  not_if "nova-manage db version && test $(nova-manage db version) -gt 0"
 end
 
-# Adding Indexing to deleted instances in Nova.
-add_index_stopgap(
-  "mysql",
-  node["nova"]["db"]["name"],
-  node["nova"]["db"]["username"],
-  node["nova"]["db"]["password"],
-  "rax_ix_reservations_deleted",
-  "reservations",
-  "deleted") do
-  action :nothing
-  subscribes :run, "execute[nova-manage db sync]", :immediately
-end
+# Adds db Indexing for the hosts as found in the agents table.
+# Defined in osops-utils/libraries
+add_index_stopgap("mysql",
+                  node["nova"]["db"]["name"],
+                  node["nova"]["db"]["username"],
+                  node["nova"]["db"]["password"],
+                  "rax_ix_reservations_deleted",
+                  "reservations",
+                  "deleted",
+                  "execute[nova-manage db sync]",
+                  :run)
 
-add_index_stopgap(
-  "mysql",
-  node["nova"]["db"]["name"],
-  node["nova"]["db"]["username"],
-  node["nova"]["db"]["password"],
-  "rax_ix_instances_deleted",
-  "instances",
-  "deleted") do
-  action :nothing
-  subscribes :run, "execute[nova-manage db sync]", :immediately
-end
+add_index_stopgap("mysql",
+                  node["nova"]["db"]["name"],
+                  node["nova"]["db"]["username"],
+                  node["nova"]["db"]["password"],
+                  "rax_ix_instances_deleted",
+                  "instances",
+                  "deleted",
+                  "execute[nova-manage db sync]",
+                  :run)
