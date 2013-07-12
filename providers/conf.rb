dd93cf7836a66a07913a15f70a891638cd767c57
git-harry <git-harry@live.co.uk>
2013-07-12 12:11:41 +0100

    Add image_cache_manager_interval as an attribute

--- providers/conf.rb
+++ providers/conf.rb
@@ -184,7 +184,8 @@ action :create do
       "ec2_listen_port" => ec2_bind["port"],
       "use_ceilometer" => node.recipe?("ceilometer::ceilometer-compute"),
       "iscsi_use_multipath" => iscsi_use_multipath,
-      "memcached_servers" => memcached_servers
+      "memcached_servers" => memcached_servers,
+      'image_cache_manager_interval' => node['nova']['config']['image_cache_manager_interval']
     )
   end
   new_resource.updated_by_last_action(t.updated_by_last_action?)
